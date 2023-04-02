fdd2cc7f1bef3cb5b4fa84f56cf205ec7649d602
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-04-02 16:29:12 +0100

    Change EditNoteViewModel.kt to use data binding

--- app/src/main/java/com/hayohtee/notes/ui/viewmodel/EditNoteViewModel.kt
+++ app/src/main/java/com/hayohtee/notes/ui/viewmodel/EditNoteViewModel.kt
@@ -9,31 +9,39 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import java.util.*
 
 class EditNoteViewModel(private val noteId: Long) : ViewModel() {
     private val repository: NoteRepository = NoteRepository.getInstance()
     private val _note: MutableStateFlow<Note> = MutableStateFlow(Note())
     val note: StateFlow<Note> = _note.asStateFlow()
 
+    val title = MutableStateFlow("")
+    val detail = MutableStateFlow("")
+
     init {
         viewModelScope.launch(Dispatchers.IO) {
             repository.getNote(noteId).collect {
                 _note.value = it
+                title.value = it.title
+                detail.value = it.note
             }
         }
     }
 
-    fun updateNote(onUpdateNote: (Note) -> Note) {
-        _note.update { oldNote ->
-            onUpdateNote(oldNote)
-        }
-    }
 
     override fun onCleared() {
         super.onCleared()
-        repository.updateNote(_note.value)
+
+        var updatedNote = note.value.copy(title = title.value, note = detail.value)
+
+        // If modified, update the date
+        if (note.value.title != title.value || note.value.note != detail.value) {
+            updatedNote = note.value.copy(title = title.value, note = detail.value, date = Date())
+        }
+
+        repository.updateNote(updatedNote)
     }
 }
 
