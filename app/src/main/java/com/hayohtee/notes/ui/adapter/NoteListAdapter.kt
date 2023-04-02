8aecc70217076ab640494a2c39fcd41631f4e8dc
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-04-02 16:30:21 +0100

    Update NoteListAdapter.kt to set view holder background and span counts

--- app/src/main/java/com/hayohtee/notes/ui/adapter/NoteListAdapter.kt
+++ app/src/main/java/com/hayohtee/notes/ui/adapter/NoteListAdapter.kt
@@ -2,8 +2,13 @@ package com.hayohtee.notes.ui.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.view.setMargins
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.hayohtee.notes.R
 import com.hayohtee.notes.data.model.Note
 import com.hayohtee.notes.databinding.NoteListItemBinding
 
@@ -17,19 +22,53 @@ class NoteListAdapter(private val onItemClick: (noteId: Long) -> Unit) :
     }
 
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
-        holder.bind(getItem(position), onItemClick)
+        holder.bind(getItem(position), position, onItemClick)
     }
 }
 
 class NoteViewHolder(private val binding: NoteListItemBinding) :
     RecyclerView.ViewHolder(binding.root) {
-    fun bind(note: Note, onItemClick: (noteId: Long) -> Unit) {
+    fun bind(note: Note, position: Int, onItemClick: (noteId: Long) -> Unit) {
         binding.apply {
             noteTitle.text = note.title
             noteDate.text = note.date.toString()
         }
         binding.root.setOnClickListener {
             onItemClick(note.id)
+        }
+
+        setBackgroundColor(position)
+        setSpan(note.title)
+    }
+
+    private fun setBackgroundColor(position: Int) {
+        val context = binding.root.context
+        binding.root.setCardBackgroundColor(
+            ContextCompat.getColor(context, getColorId(position))
+        )
+    }
+
+    private fun setSpan(title: String) {
+        val layoutParams = StaggeredGridLayoutManager.LayoutParams(binding.root.layoutParams)
+
+        if (title.length >= 45) {
+            layoutParams.isFullSpan = true
+            binding.noteDate.textAlignment = TextView.TEXT_ALIGNMENT_TEXT_END
+        }
+
+        layoutParams.setMargins(8)
+        binding.root.layoutParams = layoutParams
+    }
+
+    companion object {
+        private val colors = listOf(
+            R.color.red_200, R.color.brown_200,
+            R.color.green_200, R.color.teal_200, R.color.purple_200,
+            R.color.orange_200, R.color.brown_500, R.color.gray_300
+        )
+
+        fun getColorId(position: Int): Int {
+            return colors[position % colors.size]
         }
     }
 }
