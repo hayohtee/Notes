9df78885088d7db70bd08afa411441a6de9ba01d
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-04-02 11:50:07 +0100

    Update NoteDetailFragment.kt

--- app/src/main/java/com/hayohtee/notes/ui/NoteDetailFragment.kt
+++ app/src/main/java/com/hayohtee/notes/ui/NoteDetailFragment.kt
@@ -1,20 +1,97 @@
 package com.hayohtee.notes.ui
 
 import android.os.Bundle
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.hayohtee.notes.R
+import com.hayohtee.notes.databinding.FragmentNoteDetailBinding
+import com.hayohtee.notes.ui.viewmodel.NoteDetailViewModel
+import com.hayohtee.notes.ui.viewmodel.NoteDetailViewModelFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class NoteDetailFragment : Fragment() {
+    private val args: NoteDetailFragmentArgs by navArgs()
+    private val viewModel: NoteDetailViewModel by viewModels {
+        NoteDetailViewModelFactory(args.noteId)
+    }
+
+    private var _binding: FragmentNoteDetailBinding? = null
+    private val binding: FragmentNoteDetailBinding
+        get() = checkNotNull(_binding) {
+            "FragmentNoteDetailBinding is not inflated"
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_note_detail, container, false)
+        _binding = FragmentNoteDetailBinding.inflate(inflater, container, false)
+
+        setupActionBar()
+        setupMenu()
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
+            viewModel.note.collect { note ->
+                binding.apply {
+                    noteTitle.text = note.title
+                    noteDetail.text = note.note
+                    noteDate.text = note.date.toString()
+                }
+            }
+        }
+    }
+
+    private fun setupMenu() {
+        (requireActivity() as MenuHost).addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.note_detail_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                when (menuItem.itemId) {
+                    R.id.editNoteFragment -> {
+                        val action = NoteDetailFragmentDirections.toEditNote(args.noteId)
+                        findNavController().navigate(action)
+                    }
+                }
+                return true
+            }
+
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
+    private fun setupActionBar() {
+        (activity as AppCompatActivity).setSupportActionBar(binding.topAppBar)
+
+        val navHostFragment = (activity as AppCompatActivity).supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.topAppBar.setupWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
