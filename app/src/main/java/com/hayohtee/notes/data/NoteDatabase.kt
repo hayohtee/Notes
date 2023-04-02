c69058753d9e63abe11eb863f0cd6bbeac52f3b5
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-04-02 11:47:16 +0100

    Update NoteDatabase.kt

--- app/src/main/java/com/hayohtee/notes/data/NoteDatabase.kt
+++ app/src/main/java/com/hayohtee/notes/data/NoteDatabase.kt
@@ -4,14 +4,17 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.hayohtee.notes.data.dao.NoteDao
 import com.hayohtee.notes.data.model.Note
 
 @Database(entities = [Note::class], version = 1, exportSchema = false)
+@TypeConverters(NoteTypeConverter::class)
 abstract class NoteDatabase : RoomDatabase() {
     abstract fun noteDao(): NoteDao
 
     companion object {
+        @Volatile
         private var INSTANCE: NoteDatabase? = null
 
         fun getInstance(context: Context): NoteDatabase {
